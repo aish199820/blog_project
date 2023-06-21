636a33c9d9c4034ab76517f170add7918f33446c
aish199820 <122533555+aish199820@users.noreply.github.com>
2023-06-22 00:53:14 +0530

    Update settings.py

--- BlogProject/settings.py
+++ BlogProject/settings.py
@@ -125,6 +125,3 @@ STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
